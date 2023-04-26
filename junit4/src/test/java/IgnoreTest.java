dedd07e0b345da0b1de6235d48dad7182026f802
王凯 <wangkai0224@foxmail.com>
2023-04-26 14:24:47 +0800

    Init Commit

---
+++ junit4/src/test/java/IgnoreTest.java
@@ -0,0 +1,17 @@
+import org.junit.Ignore;
+import org.junit.Test;
+
+@Ignore
+public class IgnoreTest {
+
+	@Ignore
+	@Test
+	public void ignoredMethod() {
+		System.out.println("This is a ignored method.");
+	}
+
+	@Test
+	public void enabledMethod() {
+		System.out.println("This is a enabled method.");
+	}
+}
---
+++ junit5/src/test/java/DisabledTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+@Disabled
+public class DisabledTest {
+
+	@Disabled
+	@Test
+	void disabledMethod() {
+		System.out.println("This is a disabled method.");
+	}
+
+	@Test
+	void enabledMethod() {
+		System.out.println("This is a enabled method.");
+	}
+}
